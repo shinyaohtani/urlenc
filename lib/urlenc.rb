034e31ecf051f59dc2a0b47aae30cbbaf3b5136e
shinyaohtani <shinya_ohtani@yahoo.co.jp>
2020-05-21 18:11:42 +0900

    default: --encode

--- lib/urlenc.rb
+++ lib/urlenc.rb
@@ -9,6 +9,20 @@ module Urlenc
   require 'erb'
   require 'optparse'
 
+  DESC_DECODE = <<~DECODE
+    Decode/unescape STDIN. %22Sample%20Text%21%22 will be
+    converted to "Sample Text!"
+  DECODE
+  DESC_ENCODE = <<~ENCODE
+    Encode/escape   STDIN. "Sample Text!" will be converted
+    to %22Sample%20Text%21%22
+    *default: --encode
+  ENCODE
+  DESC_SPLIT = <<~SPLIT
+    Split before Encode/escape. http://sample Text!/ will
+    be converted to http://sample%20Text%21/
+    *default: --no-split
+  SPLIT
   # Usage: urlenc [options]
   #     -d, --decode  : Decode/unescape STDIN
   #     -e, --encode  : Encode/escape   STDIN
@@ -16,42 +30,38 @@ module Urlenc
     attr_accessor :params
 
     def initialize
-      @params = {}
+      @params = { encode: true }
     end
 
     def parse_options
       OptionParser.new do |opts|
         opts = define_options(opts)
         opts.parse!(ARGV, into: @params)
-        if @params[:decode].nil? and @params[:encode].nil?
-          puts opts.help
-        end
+        puts opts.help if @params[:decode].nil? && @params[:encode].nil?
       end
     end
 
     def run
       if @params[:decode]
         STDIN.each { |line| puts CGI.unescape(line.chomp) }
-      elsif @params[:encode]
-        if @params[:split]
-          STDIN.each do |line|
-            protocol = line.chomp!.slice!(%r{^.+://}) || ''
-            puts protocol + line.split('/', -1).map { |e| ERB::Util.url_encode(e) }.join('/')
-          end
-        else
-          STDIN.each { |line| puts ERB::Util.url_encode(line.chomp) }
+      elsif @params[:split]
+        STDIN.each do |line|
+          protocol = line.chomp!.slice!(%r{^.+://}) || ''
+          puts protocol + line.split('/', -1).map { |e| ERB::Util.url_encode(e) }.join('/')
         end
+      else
+        STDIN.each { |line| puts ERB::Util.url_encode(line.chomp) }
       end
     end
 
-    def define_options(opts)
+    def define_options(opts) # rubocop:disable Metrics/AbcSize
       opts.version = VERSION
       opts.separator ' Required:'
-      opts.on('-d', '--decode', 'Decode/unescape STDIN. %22Sample%20Text%21%22 to "Sample Text!"') { |v| v }
-      opts.on('-e', '--encode', 'Encode/escape   STDIN. "Sample Text!" to %22Sample%20Text%21%22') { |v| v }
+      opts.on('-d', '--decode', *DESC_DECODE.split(/\R/)) { |v| v }
+      opts.on('-e', '--encode', *DESC_ENCODE.split(/\R/)) { |v| v }
       opts.separator ''
       opts.separator ' Optional:'
-      opts.on('-s', '--split',  'Split before Encode/escape. http://sample Text!/ to http://sample%20Text%21/') { |v| v }
+      opts.on('-s', '--[no-]split', *DESC_SPLIT.split(/\R/)) { |v| v }
       opts.separator ''
       opts.separator ' Common options:'
       opts.on_tail('-h', '--help', 'Show this message') do
